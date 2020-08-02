06af37133c1c71f5c2790594a90ad9560f66fff7
CruzZFire <a.cruzana88@gmail.com>
2020-08-02 12:40:56 +0200

    Added fixtures to books app, base model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Book(model.Model):
+    book_id: 2767052
+    books_count: 272
+    authors: "Suzanne Collins"
+    publication_year: 2008
+    title: "The Hunger Games (The Hunger Games, #1)"
+    language_code: "eng"
+    average_rating: 4.34
+    ratings_count: 4942365
+    ratings_1: 66715
+    ratings_2: 127936
+    ratings_3: 560092
+    ratings_4: 1481305
+    ratings_5: 2706317
+    image_url: "https://images.gr-assets.com/books/1447303603m/2767052.jpg"
+  
