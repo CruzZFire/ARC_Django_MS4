b5485a48a2ae131562501444121d20831abdad68
CruzZFire <a.cruzana88@gmail.com>
2020-08-03 17:54:06 +0200

    Finished base style of the index.html, slight touch at base.html style

--- home/views.py
+++ home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from books.models import Book
 
-# Create your views here.
+
 def home(request):
     """" A view for index page """
 
+    promoted = Book.objects.filter(promoted=True)[0:11]
+
     context = {
-        
+        "promoted": promoted,
     }
 
     return render(request, 'home/index.html', context)
