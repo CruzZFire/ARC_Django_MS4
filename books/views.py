d4f57de2cd9d2e729d5988b4c235e72ef83a80d1
CruzZFire <a.cruzana88@gmail.com>
2020-08-04 22:28:12 +0200

    Styled books.html, refined urls for details.html

--- books/views.py
+++ books/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .models import Book
+
 
 def all_books(request):
     """" A view for all books page, filter and sort functions """
-    
     books = Book.objects.all().order_by('title')
 
     context = {
@@ -12,18 +15,28 @@ def all_books(request):
     
     return render(request, 'books/books.html', context)
 
-def book_detail(request):
-    """" A view for index page """
-    return render(request, 'home/index.html')
+
+def book_detail(request, book_id):
+    """" A view for book detail page """
+    book = get_object_or_404(Book, pk=book_id)
+
+    context = {
+        "book": book,
+    }
+
+    return render(request, 'books/book_detail.html', context)
+
 
 def add_book(request):
     """" A view for index page """
     return render(request, 'home/index.html')
 
-def edit_book(request):
+
+def edit_book(request, book_id):
     """" A view for index page """
     return render(request, 'home/index.html')
 
-def del_book(request):
+
+def del_book(request, book_id):
     """" A view for index page """
     return render(request, 'home/index.html')
