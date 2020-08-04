392dbf7b2135610057f6d57d59faf49ed6c61675
CruzZFire <a.cruzana88@gmail.com>
2020-08-04 11:42:21 +0200

    Added basic userprofile model and tested creation via admin

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from allauth.account.signals import user_signed_up
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(user_signed_up)
+def profile_creation(sender, **kwargs):
+    profile = UserProfile(user = kwargs['user'])
+    profile.save()
