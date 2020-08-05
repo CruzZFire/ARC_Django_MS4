fd014c389e0d59667217bbad2b25f6906682565b
CruzZFire <a.cruzana88@gmail.com>
2020-08-05 11:56:18 +0200

    Added pagination to all books view and styled it

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 from .models import Book
 
@@ -8,9 +9,19 @@ from .models import Book
 def all_books(request):
     """" A view for all books page, filter and sort functions """
     books = Book.objects.all().order_by('title')
+    paginator = Paginator(books, 24)
+    page_request = 'page'
+    page = request.GET.get(page_request)
+    try:
+        paginated_books = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_books = paginator.page(1)
+    except EmptyPage:
+        paginated_books = paginator.page(paginator.num_pages)
 
     context = {
-        "books": books,
+        "paginated_books": paginated_books,
+        "page_request": page_request,
     }
     
     return render(request, 'books/books.html', context)
