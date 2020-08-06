d0a03c04698ad9ce6384629782f5e108db62bee3
CruzZFire <a.cruzana88@gmail.com>
2020-08-06 16:07:50 +0200

    Added subscription template skeleton and url config for it. Changed calculation for rating bars.

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def subscriptions(request):
     """" A view for index page """
-    return render(request, 'home/index.html')
+    return render(request, 'subscriptions/subscriptions.html')
