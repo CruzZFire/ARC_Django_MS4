4eae605a5989671d22d35f14c1b7f05aa209b20c
CruzZFire <a.cruzana88@gmail.com>
2020-08-07 16:21:20 +0200

    Added review creation placement at book detail template

--- books/models.py
+++ books/models.py
@@ -24,3 +24,7 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+    @property
+    def get_reviews(self):
+        return self.reviews.all().order_by('datestamp')[0:4]
--- books/views.py
+++ books/views.py
@@ -5,6 +5,8 @@ from django.db.models import Q
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 from .models import Book
+from reviews.forms import ReviewForm
+from reviews.models import Review
 
 
 def all_books(request):
@@ -53,9 +55,16 @@ def search_books(request):
 def book_detail(request, book_id):
     """" A view for book detail page """
     book = get_object_or_404(Book, pk=book_id)
+    form = ReviewForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.instance.user = request.user
+            form.instance.book = book
+            form.save()
 
     context = {
         "book": book,
+        "form": form,
     }
 
     return render(request, 'books/book_detail.html', context)
