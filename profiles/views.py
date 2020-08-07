a8041770c13513c44d96d55a5b48b24761099b0f
CruzZFire <a.cruzana88@gmail.com>
2020-08-07 19:37:09 +0200

    Linked reviews logic to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import redirect, render, reverse, get_object_or_404
+
 from .models import UserProfile
 from books.models import Book
+from reviews.models import Review
 
 
 def profile(request):
     """" A view for profile page """
     profile = get_object_or_404(UserProfile, user=request.user)
+    user_reviews_found = Review.objects.filter(user_id=profile.user_id)
+    user_reviews_count = user_reviews_found.count()
+    user_reviews_short = user_reviews_found.order_by('-review_id')[0:3]
+    short_reviews_count = user_reviews_short.count()
 
     context = {
         "profile": profile,
+        "user_reviews_found": user_reviews_found,
+        "user_reviews_count": user_reviews_count,
+        "user_reviews_short": user_reviews_short,
+        "short_reviews_count": short_reviews_count,
     }
 
     return render(request, 'profiles/profile.html', context)
