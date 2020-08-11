ea841881fee071e1c3d1450ff792263575452e4a
CruzZFire <a.cruzana88@gmail.com>
2020-08-11 16:30:07 +0200

    Added read list view and template, wired functionality and styled

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('edit/<username>', views.edit_profile, name='edit_profile'),
     path('update/<username>', views.update_profile, name='update_profile'),
+    path('user_books/<username>', views.user_books, name='user_books'),
     path('ask_book/<int:book_id>/', views.ask_book, name='ask_book'),
+    path('rmv_book/<int:book_id>/', views.rmv_book, name='rmv_book'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -17,10 +17,14 @@ def profile(request):
     user_reviews_short = user_reviews_found.order_by('-review_id')[0:3]
     short_reviews_count = user_reviews_short.count()
     books_in_profile = profile.asked_books.all()
+    read_list_size = 4 - books_in_profile.count()
+    empty_loop = range(0, read_list_size)
 
     context = {
         "profile": profile,
         "books_in_profile": books_in_profile,
+        "read_list_size": read_list_size,
+        "empty_loop": empty_loop,
         "user_reviews_found": user_reviews_found,
         "user_reviews_count": user_reviews_count,
         "user_reviews_short": user_reviews_short,
@@ -72,23 +76,55 @@ def update_profile(request, username):
             messages.error(request, 'Error Changing Avatar')
             return redirect(reverse('profile'))
 
-    return redirect(reverse('profile'))
+    return redirect(reverse('profiles/profile'))
+
+
+@login_required
+def user_books(request, username):
+    """" Manage profile books for user """
+    user_profile = get_object_or_404(UserProfile, user__username=username)
+    if not request.user.username == user_profile.user.username:
+        messages.error(request, 'Sorry, that is not your profile')
+        return redirect(reverse('profile'))
+
+    books_in_profile = user_profile.asked_books.all()
+    read_list_size = 4 - books_in_profile.count()
+    empty_loop = range(0, read_list_size)
+
+    context = {
+        "books_in_profile": books_in_profile,
+        "empty_loop": empty_loop,
+    }
+
+    return render(request, 'profiles/user_books.html', context)
 
 
 @login_required
 def ask_book(request, book_id):
     """" Get or Create books in asked-books """
     book = Book.objects.get(book_id=book_id)
-    user = get_object_or_404(UserProfile, user=request.user)
-    user_book_count = user.asked_books.count()
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    user_book_count = user_profile.asked_books.count()
 
-    if user_book_count < 4:
-        if user.asked_books.get(book_id=book.book_id) is None:
-            messages.success(request, 'Book added to your read list')
-            return redirect(request, "url 'book_detail' book.book_id")
-        else:
-            messages.error(request, 'Book is already on your read list')
+    if user_profile.asked_books.filter(book_id=book.book_id):
+        messages.error(request, 'Already on your read list')
     else:
-        messages.error(request, 'Already 4 books on your read list')
+        if user_book_count < 4:
+            user_profile.asked_books.add(book)
+            messages.success(request, 'Book added to your read list')
+        else:
+            messages.error(request, 'Read list full!!')
 
-    return render(request, 'home/index.html')
+    return redirect(reverse('profile'))
+
+
+@login_required
+def rmv_book(request, book_id):
+    """" Remove books in asked-books """
+    book = Book.objects.get(book_id=book_id)
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+
+    user_profile.asked_books.remove(book)
+    messages.success(request, 'Book removed from your read list')
+
+    return redirect(reverse('profile'))
